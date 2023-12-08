c255396dff1fbecc2d61b10416ced444eeaeacc0
David Jeong <drumrobot43@gmail.com>
2023-12-08 22:13:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    for (let i = 0; i < 500; i++) {
+        if (num === 1) {
+            return i;
+        }
+        if (num % 2) {
+            num = num * 3 + 1;
+        } else {
+            num /= 2;
+        }
+    }
+    return  -1;
+}
